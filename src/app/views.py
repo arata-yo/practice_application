de403d4b4aefa325f0d4ea0682672840362e1e76
root <root@UBUNTU>
2025-08-11 16:39:15 +0900

    change files

--- src/app/views.py
+++ src/app/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from .models import Area, Cafe, Utility
 
-# Create your views here.
-# def index(request):
-#     return render(request, 'index.html')
-def index(request):
-    return HttpResponse("Hello, world! This is the index page.")
+class IndexView(generic.ListView):
+    template_name = 'list.html'
+    context_object_name = 'cafe_list'
+    def get_queryset(self):
+        """Return the last five published records."""
+        return Cafe.objects.order_by('name')[:5]
+class DetailView(generic.DetailView):
+    model = Cafe
+    template_name = 'detail.html'
